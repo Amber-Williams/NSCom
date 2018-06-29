c9b3aff4762f4f24648be52ae6190f54d6cd452d
Amber Williams <amberswebdesign@gmail.com>
2018-06-29 13:13:25 -0500

    Created Advanced-Day page

--- Advanced-Day/index.php
+++ Advanced-Day/index.php
@@ -8,7 +8,7 @@
 
       <div class="col-2 d-flex flex-column"> 
         <div class="p-2 hide-md">
-          <img src="../img/nc-indi-product1.jpg" class="float-left" onclick="pictureChangeFirst()"/>
+          <img src="../img/dc-indi-product1.jpg" class="float-left" onclick="pictureChangeFirst()"/>
         </div>
         <div class="p-2 hide-md">
           <img src="../img/nc-indi-product2.jpg" class="float-left" onclick="pictureChangeSecond()"/>
@@ -20,14 +20,14 @@
 
       <div class="col-md-5">
         <div class="d-flex align-items-center indi-display">
-          <img id="indi-display-img" src="../img/Updated-Advanced-Packaging/NSC.ADV.NC.BottleAndCarton.HR.CMYK.V1.png" class="float-right"/>
+          <img id="indi-display-img" src="../img/Updated-Advanced-Packaging/NSC.ADV.DC.BottleAndCarton.HR.CMYK.V1.png" class="float-right"/>
         </div>
       </div>
 
       <div class="row show-md mx-auto">
         <div class="col-12 d-flex">
             <div class="p-2">
-              <img src="../img/nc-indi-product1.jpg" class="indi-pictureChanger" onclick="pictureChangeFirst()"/>
+              <img src="../img/dc-indi-product1.jpg" class="indi-pictureChanger" onclick="pictureChangeFirst()"/>
             </div>
             <div class="p-2">
               <img src="../img/nc-indi-product2.jpg" class="indi-pictureChanger" onclick="pictureChangeSecond()"/>
@@ -39,18 +39,18 @@
       </div>
 
       <div class="col-md-5">
-        <h1 class="h1-indi">ADVANCED NIGHTLY FACE CREAM</h1>
+        <h1 class="h1-indi text-uppercase">Advanced Daily Face Cream</h1>
         <h4 class="h4-indi">30ml / 1 fl.oz</h4>
         <h2 class="h2-indi mb-4">$120<h2>
             
         <h3 class="h3-indi benefits-line">BENEFITS</h3>
         <ul class="h4-indi ul-hexbullets">
-          <li>Skin Protectant</li>
-          <li>Fine Lines and Wrinkles</li>
-          <li>Sagging Skin</li>
-          <li>Restores Radiance Over-night</li>
-          <li>Fights Signs of Aging</li>
-          <li>Blemised and Sun Damaged Skin</li>
+          <li>Sun Protection</li>
+          <li>Moisturizing and Normalizing</li>
+          <li>Light-Weight</li>
+          <li>Restores Youthful Appearance</li>
+          <li>Enlarged Pores</li>
+          <li>Discoloration</li>
         </ul>
         <button>SHOPIFY BUTTON HERE</button>
       </div>
@@ -85,7 +85,7 @@
       <div class="col-md-7">
         <h3 class="h3-indi">Product Details</h3>
         <p class="p-indi">
-        Advanced Nightly Face Treatment is a rich, concentrated age-defying skin protection formula designed to fight oxidative stress and improve the appearance of aging and sun damaged skin while promoting healthy skin of face, neck, and décolletage. This treatment is infused with a matrix of skin revitalizing key ingredients which work in synergy to reduce the appearance of fine lines and wrinkles, enhance and restore your skin’s ability to look fresh, natural, and radiant overnight.
+        Advanced Daily Face Cream is a multi-dimensional skin moisturizing formula designed to help protect skin against sun damage/environmental stressors, improve moisture retention and skin hydration while helping to normalize and restore skin to its youthful appearance. For application to the face, neck, and other areas of the body, this skin protecting and hydrating day cream combines revolutionary key ingredients that actively work together to awaken and refresh your skin’s youthful, luminous tone.
         <br/><br/>
         Dermatologist Tested.
         <br/><br/>
@@ -95,7 +95,7 @@
       <div class="col-md-5 bg-c-grey">
       <h2 class="h2-indi">ADVANCED COLLECTION</h2>
         <p class="p-indi">Introduce night cream and body cream here with stylized group shot.</p>
-        <button>See Daily Cream</button>  <button>See Body Cream</button>
+        <button>See Night Cream</button>  <button>See Body Cream</button>
       </div>
     </div>
 
@@ -140,7 +140,7 @@
                 <img src="../img/indi-stuff/Step2.png"/>
               </div>
               <div class="col-sm-10 d-flex align-items-center">
-                <p class="p-indi mb-0">Apply as needed. Recommended to apply four (+/-) pumps to face, neck and décolletage.</p>
+                <p class="p-indi mb-0">Apply generously before sun exposure and as needed.</p>
               </div>
             </div>
             <div class="row mb-3">
@@ -148,14 +148,14 @@
                 <img src="../img/indi-stuff/Step3.png"/>
               </div>
               <div class="col-sm-10 d-flex align-items-center">
-                <p class="p-indi mb-0">Rinse skin in the morning following usage. For optimal results, follow with Nerium ADVANCED Daily Face Cream.</p>
+                <p class="p-indi mb-0">Reapply throughout day as needed. For optimal results, follow with Nerium ADVANCED Nightly Face Treatment.</p>
               </div>
             </div>
           </div>
           <div class="col-md-5 bg-c-grey">
             <h2 class="h2-indi">ADVANCED COLLECTION</h2>
             <p class="p-indi">Introduce night cream and body cream here with stylized group shot.</p>
-            <button>See Daily Cream</button>  <button>See Body Cream</button>
+            <button>See Night Cream</button>  <button>See Body Cream</button>
           </div>
         </div>
       </div>
@@ -169,26 +169,27 @@
             <p class="p-indi ">Targets not just the signs of aging but also the underlying cause. Delivers powerful antioxidants to target free radical damage. Boosts the skin rejuvenating process to reveal younger-looking skin.</p>
           </div>
           <div class="col-sm-6 col-md-3">
-            <img src="../img/indi-stuff/ingredients/advanced/NB-Peptide-Matrix.png" alt="BiovaDerm"/>
-            <p class="p-indi">This advanced technology process retains the natural ratios of key elements found in nature’s potent life source of proteins and peptides from water soluble egg membrane (WSEM) that are beneficial to skin.</p>
+            <img src="../img/indi-stuff/ingredients/advanced/D-Zinc-Oxide.png" alt="Zinc Oxide"/>
+            <p class="p-indi">This popular ingredient is used primary to shield and protect the skin from UVB, UVA II and UVA I rays, as it is considered as one of the safest to use sun-protecting ingredient on the market. Zinc oxide is also used to be a soothing agent and an antioxidant.</p>
           </div>
           <div class="col-sm-6 col-md-3">
-            <img src="../img/indi-stuff/ingredients/advanced/DN-Niacinamide.png" alt="Nerium Oleander Extract"/>
+            <img src="../img/indi-stuff/ingredients/advanced/DN-Niacinamide.png" alt="Niacinamide"/>
             <p class="p-indi">Vitamin B3</p>
           </div>
           <div class="col-sm-6 col-md-3">
-            <img src="../img/indi-stuff/ingredients/advanced/NB-Dimethicone.png" alt="Nerium Oleander Extract"/>
-            <p class="p-indi">One of the most commonly found skin care ingredient is a silicon-based polymer used to condition the skin while protecting the skin, working to prevent water loss by forming a hydrating barrier on the skin. Fills in wrinkles and lines on the face to give it a temporary plumped appearance.</p>
+            <img src="../img/indi-stuff/ingredients/advanced/D-Hyaluronic-Acid.png" alt="Hyaluronic Acid"/>
+            <p class="p-indi">Powerful skin moisturizer</p>
           </div>
           <div class="col-sm-6 col-md-3">
-            <img src="../img/indi-stuff/ingredients/advanced/N-Chia-Seed-Oil.png" alt="Nerium Oleander Extract"/>
-            <p class="p-indi">Text here text here</p>
+            <img src="../img/indi-stuff/ingredients/advanced/D-SD.png" alt="Superoxide Dismutase"/>
+            <p class="p-indi">Antioxidant Enzyme</p>
           </div>
         </div>
         <div class="row mb-5">
           <div class="col-12">
             <h3 class="h3-indi">Full ingredients</h3> 
-            <p class="p-indi">Dimethicone, Water (Aqua), Aloe Barbadensis Leaf Juice, Hydrolyzed Egg Shell Membrane (Peptide Matrix - Collagen and Elastin), Nerium Oleander Leaf Extract, Niacinamide, Glycerin, Propanediol, Butylene Glycol, Glyceryl Stearate, Hydroxyethyl Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Hydrogenated Polydecene, Cocos Nucifera (Coconut) Oil, Caprylic/Capric Triglyceride, Helianthus Annuus (Sunflower) Seed Oil, Squalane, Cetearyl Olivate, Sorbitan Olivate, C12-15 Alkyl Benzoate, Stearic Acid, Leuconostoc/Radish Root Ferment Filtrate, Butyrospermum Parkii (Shea) Butter, Theobroma Cacao (Cocoa) Seed Butter, Polysorbate 80, Caprylyl Glycol, Salvia Hispanica (Chia) Seed Oil, Rosmarinus Officinalis (Rosemary) Leaf Extract, Sodium Lactate, Tocopheryl Acetate, Xanthan Gum, Cymbopogon Schoenanthus Extract, Disodium EDTA, Acetyl Hexapeptide-30, Alteromonas Ferment Extract, Arginine, Caramel, Potassium Sorbate, Glyceryl Caprylate, Phenylpropanol, Limonene, Citral, Hydroxycitronellal, Linalool</p>
+            <p class="p-indi">Zinc Oxide, <br/>
+            Water, C12-15 Alkyl Benzoate, Glycerin, Aloe Barbadenis Leaf Juice, Cetearyl Alcohol, Ceteth-20 Phosphate, Oleth-3 Phosphate, Isostearic Acid, Silica, Nerium Oleander Leaf Extract, Hydrolyzed Egg Shell Membrane, (Peptide Matrix - Collagen & Elastin), Caprylyl Glycol, Leuconostoc/Radish Root Ferment Filtrate, Superoxide Dismutase, Polygonum Aviculare Extract, Dimethicone, Cetearyl Glucoside, Niacinamide, Sodium Hyaluronate, Tetrahexyldecyl Ascorbate, Glyceryl Caprylate, Avena Sativa (Oat) Kernel Extract, Chamomilla Recutita (Matricaria) Flower Extract, Rosmarinus Officinalis (Rosemary) Leaf Extract, Tocopheryl Acetate, Dipotassium Glycyrrhizinate, Palmitoyl Dipeptide-5 Diaminobutyroyl Hydroxythreonine, Palmitoyl Tripeptide-5, Tetradecyl Aminobutyroylvalylaminobutyric Urea Trifluoroacetate, Glyceryl Stearate Citrate, Magnesium Chloride, Xanthan Gum, Disodium EDTA, Phenylpropanol, Dicetyl Phosphate, Polyhydroxystearic Acid, Potassium Sorbate, Fragrance, Mica, Titanium Dioxide, Pentylene Glycol, Phenoxyethanol, Sodium Benzoate, Tromethamine</p>
           </div>
         </div>
       </div>
